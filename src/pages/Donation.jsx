db29a4834c50125e3b89f6a28062590c1789c496
muntasir-mehedi <muntasirmehedi95@gmail.com>
2023-09-27 22:09:09 +0600

    donetion added

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import Card from "../components/card/Card";
 
 
 const Donation = () => {
+
+    const [donation, setDonetion] = useState();
+    const [noData, setNoData] = useState();
+    useEffect(()=>{
+
+        const donateItems = JSON.parse(localStorage.getItem('donation'))
+        if (donateItems) {
+            setDonetion(donateItems)
+        } else {
+            setNoData('No Data')
+        }
+    },[])
+
     return (
         <div>
-            
+            {noData ? <p className="flex justify-center items-center">{noData}</p> 
+            : 
+            <div>
+
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-5 py-8">
+                    {donation?.map(card=><Card key={card.id} card={card}></Card>)}
+                </div>
+
+            </div>}
         </div>
     );
 };
